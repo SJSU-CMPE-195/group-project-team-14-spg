ffc45363dfc0ec0ad15a05fd76ca0493ee587fa4
antmtn <116135015+notnuthin@users.noreply.github.com>
2026-05-04 00:48:27 -0700

    changed playwright config to point at moved directory

--- frontend/playwright.config.js
+++ frontend/playwright.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from '@playwright/test'
 
 export default defineConfig({
-  testDir: './test_e2e',
+  testDir: './tests/e2e',
   use: {
     baseURL: 'http://127.0.0.1:4173',
     trace: 'on-first-retry',
